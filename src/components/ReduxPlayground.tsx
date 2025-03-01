5af0846d74d145d00c61066484b9d7bddcd312a8
Mohammad Moaid <79970218+moe-moaid@users.noreply.github.com>
2025-03-01 20:01:56 +0500

    added async actions to redux (#1)

--- src/components/ReduxPlayground.tsx
+++ src/components/ReduxPlayground.tsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import ThemeButton from './ThemeButton';
-import { RootState } from 'src/redux/store';
-import { numberIncrement, numberDecrement } from 'src/redux/theme/themeSlice';
+import { AppDispatch, RootState } from 'src/redux/store';
+import { numberIncrement, numberDecrement, incrementAsync, decremenAsync } from 'src/redux/theme/themeSlice';
 export default function ReduxPlayground() {
   const num = useSelector((state: RootState) => state.theme.num);
-  const dispatch = useDispatch();
+  const asyncNum = useSelector((state: RootState) => state.theme.asyncNum);
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <>
       <div>
@@ -23,6 +24,23 @@ export default function ReduxPlayground() {
           <button
             className="border border-yellow-300 px-2 py-1 rounded-md"
             onClick={() => dispatch(numberDecrement(num - 10))}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+      <div>
+        <p>click to Increment the number Asyncronously {asyncNum}</p>
+        <div className="flex flex-row gap-x-4">
+          <button
+            className="border border-green-300 px-2 py-1 rounded-md"
+            onClick={() => dispatch(incrementAsync(asyncNum + 10))}
+          >
+            Increment
+          </button>
+          <button
+            className="border border-yellow-300 px-2 py-1 rounded-md"
+            onClick={() => dispatch(decremenAsync(asyncNum - 10))}
           >
             Decrement
           </button>
