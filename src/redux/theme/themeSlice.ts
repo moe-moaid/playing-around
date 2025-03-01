fc044b27be0bd63a0d5cc8820da3ac797c19a030
Moe_Moaid <mohammadmoaid@gmail.com>
2025-03-01 20:00:00 +0500

    added async actions to redux

--- src/redux/theme/themeSlice.ts
+++ src/redux/theme/themeSlice.ts
@@ -1,13 +1,15 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface ThemeState {
   theme: string;
   num: number;
+  asyncNum: number;
 }
 
 const initialState: ThemeState = {
   theme: 'light',
   num: 10,
+  asyncNum: 10,
 };
 
 const themeSlice = createSlice({
@@ -24,7 +26,33 @@ const themeSlice = createSlice({
       state.num = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, () => {
+      console.log("incrementing in pending");
+      
+    }).addCase(incrementAsync.fulfilled, (state, action: PayloadAction<number>) => {
+      state.asyncNum = action.payload;
+    })
+  }
 });
+
+
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  }
+);
+
+export const decremenAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  }
+);
 
 export const { setTheme, numberIncrement, numberDecrement } =
   themeSlice.actions;
