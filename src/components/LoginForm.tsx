65a621b2a43ed9ac48b2265379e0cbc72918e615
Mohammad Moaid <mohammadmoaid@gmail.com>
2025-04-16 19:35:12 +0500

    urgently done

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -18,9 +18,9 @@ function LoginForm() {
   return (
     <div className="flex justify-center mt-24 width-[400px]">
       <form action="" className="w-[280px]">
-        {/* <div className="flex flex-col justify-start">
+        <div className="flex flex-col justify-start">
           <Input type="text" placeholder="userName" required={true} />
-        </div> */}
+        </div>
         <div className="flex flex-col mt-4">
           <Input type="password" placeholder="password" required={true} />
         </div>
